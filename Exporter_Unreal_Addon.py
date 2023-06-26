51bb02413a049145b85f49c69e1bc06b4e045c31
Tarmunds <137815720+Tarmunds@users.noreply.github.com>
2023-06-26 18:49:01 +0200

    Code push

---
+++ Exporter_Unreal_Addon.py
@@ -0,0 +1,111 @@
+import bpy
+import os
+from bpy.types import Operator
+
+bl_info = {
+    "name": "Export Unreal",
+    "author": "Tarmund",
+    "version": (2, 7),
+    "blender": (2, 80, 0),
+    "location": "File > Export",
+    "description": "Exports each selected object into its own file, at the origin of the world (depend of objects origins)",
+    "category": "Import-Export",
+}
+
+# try de pannel avec variable
+class MeshRenamePanel(bpy.types.Panel):
+    bl_idname = "VIEW3D_PT_path_var"
+    bl_label = "Path Var"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
+    
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column(align=True)
+        col.prop(context.scene, "mesh_rename_path", text="Path Var")
+        col.operator("export.selected_object", text="export")
+
+#fonction d'export type ue
+class ExportSelectedObjectsOperator(Operator):
+    bl_idname = "export.selected_object"
+    bl_label = "Export Unreal"
+
+    def execute(self, context):
+
+        path = context.scene.mesh_rename_path
+
+        # export to blend file location
+        basedir = os.path.dirname(bpy.data.filepath)
+
+        if not basedir:
+            raise Exception("Blend file is not saved")
+
+        exportDir = path
+        view_layer = context.view_layer
+
+        obj_active = view_layer.objects.active
+        selection = context.selected_objects
+
+        bpy.ops.object.select_all(action='DESELECT')
+
+        for obj in selection:
+            obj.select_set(True)
+
+            # some exporters only use the active object
+            context.view_layer.objects.active = obj
+
+            # allow location static override
+            # bpy.ops.object.make_override_static()
+            bpy.ops.object.make_local()
+
+            # store object location then zero it out
+            location = obj.location.copy()
+            bpy.ops.object.location_clear()
+
+            name = bpy.path.clean_name(obj.name)
+            fn = os.path.join(exportDir, name)
+
+            bpy.ops.export_scene.fbx(filepath=fn + ".fbx", use_selection=True, apply_unit_scale=False,
+                                     object_types={'MESH', 'ARMATURE'}, mesh_smooth_type='FACE',
+                                     use_mesh_modifiers=True)
+
+            # restore location
+            obj.location = location
+
+            obj.select_set(False)
+
+            print("written:", fn)
+
+        view_layer.objects.active = obj_active
+
+        for obj in selection:
+            obj.select_set(True)
+
+        return {'FINISHED'}
+
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportSelectedObjectsOperator.bl_idname, text="Export Selected Unreal Ready (fbx)")
+
+
+def register():
+    bpy.utils.register_class(ExportSelectedObjectsOperator)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export) #base export end
+    bpy.utils.register_class(MeshRenamePanel)
+    bpy.utils.register_class(MeshRenameOperator)
+    bpy.types.Scene.mesh_rename_path = bpy.props.StringProperty(name="path", default="")
+
+
+
+def unregister():
+    bpy.utils.unregister_class(ExportSelectedObjectsOperator)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export) #base export end
+    bpy.utils.unregister_class(MeshRenamePanel)
+    bpy.utils.unregister_class(MeshRenameOperator)
+    del bpy.types.Scene.mesh_rename_path
+    del bpy.types.Scene.mesh_rename_increment
+
+
+if __name__ == "__main__":
+    register()
---
+++ README.txt
@@ -0,0 +1,22 @@
+Unreal Exporter
+
+Fonctions : 
+
+This addon will export separately all your selected meshes at the world origin, based on their own origins. Typically if you want to import on unreal with a quick drag and drop, you will have the right origins for each objects, and they will keep it at each reimport (if you exported with the addon)
+basics :
+
+----------------------------------------------
+
+set a path : 
+	In the 3d viewport, open the tool menu (N) , go to tool, and set a path of an existing folder (create the folder before using the script, if not, it will not work), you dont need to transform it (you can keep spacebar, …).
+ 
+Export :
+	You have 2 ways to export with this add-on, using huge button export under the panel, or in the export classic panel. Plus, you can add a shortcut to it by right clicking on it and “assign shortcut”.
+
+----------------------------------------------
+
+Credit : 
+
+*collègue de sacha* : Original creator of the script
+Sacha Veyrier : modification and tweak of the script
+Kostia Perry : Add-on creator, and variable path creator
